d810063e3ac3f550b31e22a67663b5df63b30ad5
Morarin1 <vovadorosenko1@gmail.com>
2023-01-15 16:26:09 +0200

    shell fix

--- doroshenko_fe-01_danilov_fe-01_cp6/shell.py
+++ doroshenko_fe-01_danilov_fe-01_cp6/shell.py
@@ -1,20 +1,12 @@
 from run import run
-import readline
 
 text = ''
 while True:
-    line = input('shit_DB > ')
-    buffer = readline.get_line_buffer()
-    if buffer:
-        line += '\n' + buffer
-    text += line
-
-    if not line.strip():
-        try:
-          result, error = run(text)
-          text = ''
-          if error:
-              print(error.as_string())
-        except Exception as e:
-          print("An error occurred:", e)
-          continue
+    try:
+      text = input('shit_DB > ')
+      result, error = run(text)
+      if error:
+          print(error.as_string())
+    except Exception as e:
+      print("An error occurred:", e)
+      continue
